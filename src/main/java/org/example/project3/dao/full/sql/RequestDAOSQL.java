86c011b3c29f2584f74e46e860697373548ad62d
marco <marcod3santis@gmail.com>
2025-05-19 18:55:54 +0200

    aggiunta reservationReq sistemazione dao + query + ROLE

--- src/main/java/org/example/project3/dao/full/sql/RequestDAOSQL.java
+++ src/main/java/org/example/project3/dao/full/sql/RequestDAOSQL.java
@@ -5,12 +5,14 @@ import org.example.project3.exceptions.DAOException;
 import org.example.project3.exceptions.DbOperationException;
 import org.example.project3.model.*;
 import org.example.project3.query.RequestQuery;
+import org.example.project3.utilities.enums.Role;
 
 
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.jar.Attributes;
 
 public class RequestDAOSQL implements RequestDAO {
     private static final String ID = "id";
@@ -25,6 +27,7 @@ public class RequestDAOSQL implements RequestDAO {
     private static final String SURNAME = "surname";
     private static final String GENDER = "gender";
     private static final String EMAIL = "email";
+
 
 
     @Override
@@ -83,13 +86,21 @@ public class RequestDAOSQL implements RequestDAO {
     }
 
     @Override
-    public void retrieveCourseRequest(Trainer trainer, List<Reservation> reservationList){
+    public void retrieveCourseRequest(Course course, List<ReservationReq> reservationList){
 
         try(Connection conn = ConnectionSQL.getConnection()){
-            ResultSet rs = RequestQuery.retireveCourseRequest(conn, trainer.getCredentials().getMail());
+            ResultSet rs = RequestQuery.retireveCourseRequest(conn, course.getCourseName());
             while(rs.next()){
+                Customer customer = new Customer(
+                        new Credentials(rs.getString(EMAIL), Role.CLIENT),
+                        rs.getString(NAME),
+                        rs.getString(SURNAME),
+                        rs.getString(GENDER),
+                        //ONLINE - OFFLINE
+                );
 
-                // finire
+                ReservationReq reservationReq = new ReservationReq (customer, course, rs.getDate("datetime").toLocalDate());
+                reservationList.add(reservationReq);
             }
 
         } catch (SQLException e) {
